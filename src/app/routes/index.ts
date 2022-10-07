42bd9d6324d1b332d29cd568e3616f6ab23e3688
David del Puerto <code@daviddelpuerto.dev>
2022-10-07 15:34:02 +0200

    Register routes retrieving services tagged as controller from container

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,32 @@
-import { Router } from 'express';
-import apiRouter from './api.router';
+import express, { Express } from 'express';
+import { container } from '../dependency-injection';
+import { Router } from '../../Shared/infrastructure/Router';
+import { RoutesMetadataKeys } from '../../Shared/infrastructure/decorators/Routes';
+import Logger from '../../Shared/infrastructure/Logger';
 
-const router = Router();
+const logger = new Logger('Router');
 
-router.use('/api', apiRouter);
+export async function registerRoutes(server: Express) {
 
-export default router;
+  const controllerServices = container.findTaggedServiceIds('controller');
+
+  for (const controller of controllerServices) {
+    const router = express.Router();
+
+    const ControllerClass = controller?.definition?.Object;
+
+    const controllerInstance = container.get(controller.id);
+
+    const basePath: string = Reflect.getMetadata(RoutesMetadataKeys.BASE_PATH, ControllerClass);
+    const routers: Router[] = Reflect.getMetadata(RoutesMetadataKeys.ROUTERS, ControllerClass);
+
+    routers.forEach(({ method, path, handlerName }) => {
+      router[method](path, controllerInstance[String(handlerName)].bind(controllerInstance));
+
+      logger.info(`Registered route: {${method.toLocaleUpperCase()}, ${basePath}${path}}`);
+    });
+
+    server.use(basePath, router);
+  }
+
+}
